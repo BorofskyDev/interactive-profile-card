ae5fd650e9cb063b1280baec73750426beb472ab
solovyovsdom <joelborofsky@mac.com>
2022-11-10 12:43:29 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.scss'
+import Body from './components/Body/Body'
+import Header from './components/Header/Header'
+
+function App() {
+ 
+
+  return (
+    <div className='Card'>
+      <Header />
+      <Body />
+    </div>
+  )
+}
+
+export default App
