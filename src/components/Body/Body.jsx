366c8a3e1c4a5d4f4aa7026ad271b48e2251b405
solovyovsdom <joelborofsky@mac.com>
2022-11-10 18:49:17 -0600

    finalized styling

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Tabs from '../TabComponent/Tabs'
+import './Body.scss'
 
 export default function Body() {
   return (
     <div className='Body-el'>
+      <h1 className="Body-el__title">Jane Doe</h1>
       <Tabs />
     </div>
   )
